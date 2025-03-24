e0442888d3b0f06f539791cf07c54fefa8751b49
Divyyasha <divtech30@gmail.com>
2025-03-25 01:36:14 +0530

    Delete source/API directory

--- source/API/TopStoriesAPI/Business/IStoryService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TopStoriesAPI.Models;
-
-namespace TopStoriesAPI.Business
-{
-    public interface IStoryService
-    {
-        Task<StoryListResponse> GetStoriesAsync(int page = 1, int pageSize = 10, string searchTitle = null);
-    }
-}
